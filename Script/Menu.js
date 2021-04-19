291ba9093861ca85e8ae850abdb32918709a090b
Weelie92 <72354254+Weelie92@users.noreply.github.com>
2021-04-20 01:17:44 +0200

    Mastermind V0.2
    La til Menu.js

---
+++ Script/Menu.js
@@ -0,0 +1,19 @@
+function ButtonCheckAnswer() {
+  //Button check answer
+}
+
+function ButtonNewGame() {
+  //New game
+}
+
+function PanelHideAnswer() {
+  //Panel hide answer
+}
+
+function ButtonCheat() {
+  //Button cheat
+}
+
+function ColorHint() {
+  //Color hint
+}
